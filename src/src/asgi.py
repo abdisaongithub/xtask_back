049f19c35fb1c4ae89c88c08f033a22518a75718
abdisa <abdtsegaye@gmail.com>
2021-09-10 02:49:27 +0600

    django, channels, redis, drf, pillow, psypcog2-binary installed and configured'

---
+++ src/src/asgi.py
@@ -0,0 +1,10 @@
+import os
+from channels.routing import ProtocolTypeRouter
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    # Just HTTP for now. (We can add other protocols later.)
+    # todo: add websocket routes here
+})
